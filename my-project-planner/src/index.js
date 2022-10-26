be7ca4532c491b17248e1e5605ecf8e69a734892
abdallahaac <aceydesignz@gmail.com>
2022-10-26 16:41:25 -0400

    updated

--- my-project-planner/src/index.js
+++ my-project-planner/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { legacy_createStore as createStore, legacy_createStore } from "redux";
+import {
+	legacy_createStore as createStore,
+	legacy_createStore,
+	applyMiddleware,
+} from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-const store = legacy_createStore(rootReducer);
+const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
